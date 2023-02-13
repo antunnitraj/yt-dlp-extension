00fae9aafb80c61b8d8cbed1a04ae3a4afeda626
Antun Nitraj <antnitraj@gmail.com>
2023-02-13 01:33:20 +0100

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,4 @@
 chrome.browserAction.onClicked.addListener((tab) => {
-    console.log(tab);
     chrome.tabs.executeScript(tab.id, {
         code: 'document.location.href = "yt-dlp://" + location.href'
     })
